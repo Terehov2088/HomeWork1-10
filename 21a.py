a39acb7288040a257b89611485c91437749224cc
Vladimir_T <42174061+Terehov2088@users.noreply.github.com>
2018-08-27 19:24:18 +0300

    Выполнил

--- 21a.py
+++ 21a.py
@@ -8,18 +8,20 @@ def get_max_digit(number): # returns int
 
 def get_max_digit1(number):
     biggest_number = 0
-    number1 = (''.join([random.choice(str('0123456789')) for x in range(number)]))
-    print(type(number1))
-    print('12 digit number: %s' % number1)
 
-    for char in number1:
+    for char in number:
         char = int(char)
-        print(char)
         if char > biggest_number:
             biggest_number = char
     return biggest_number
 
-result = get_max_digit1(12)
+
+number = (''.join([random.choice(str('0123456789')) for x in range(12)]))
+print(type(number))
+
+
+print('12 digit number: %s' % number)
+result = get_max_digit1(number)
 print('Biggest number of 12 digit number is number: %d' % result)
 
 
