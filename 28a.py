b47b68cacdb4b7e7ffd050209fa987b6c1ee4219
Vladimir_T <42174061+Terehov2088@users.noreply.github.com>
2018-09-03 15:49:09 +0300

    Выполнил

--- 28a.py
+++ 28a.py
@@ -36,7 +36,7 @@ def input_from_keyboard():
     return list1
 
 str_to_encode = input_from_keyboard()
-print(str_to_encode)
+print("Слова до дешифровки: ", str_to_encode)
 
 
 # str_to_encode = input("Введите слово для закодировки: ")
@@ -44,7 +44,6 @@ print(str_to_encode)
 
 def encode(str_to_encode):
     password_sequence = string.ascii_lowercase + string.digits
-    print(password_sequence)
     encode_word = ""
     list_encode = []
     idx = 0
@@ -76,23 +75,6 @@ def encode(str_to_encode):
         encode_word = ""
     return list_encode
 
-print(encode(str_to_encode))
+print("Слова после дефировки: ",  encode(str_to_encode))
 
-
-        # for j in str_to_encode[i]:
-        #     if ord(j) >= ord("v"):
-        #         j = chr(ord(j) - 21)
-        #         print(j)
-        #     else:
-        #         j = chr(ord(j) + 5)
-        #         print(j)
-        # print(str_to_encode)
-
-
-
-
-
-# password_sequence = string.ascii_lowercase
-# print(type(password_sequence))
-# print(password_sequence)
 
