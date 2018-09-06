57a3499806190678f4cb3a162d48516675c4a336
Vladimir_T <42174061+Terehov2088@users.noreply.github.com>
2018-09-06 22:03:14 +0300

    Выполнил

--- 30.py
+++ 30.py
@@ -3,3 +3,29 @@
 
 def gen_primes(): # returns list of ints
 """)
+
+
+
+n = int(input("Введите колличество чисел: "))
+
+
+def gen_primes(n):
+    natural_numbers = list(range(2, n + 1))
+    for number in natural_numbers:
+        if number != 0:
+            for candidate in range(2 * number, n + 1, number):
+                natural_numbers[candidate - 2] = 0
+    primes = []
+    for number in natural_numbers:
+        if number != 0:
+            primes.append(number)
+    return primes
+
+print(gen_primes(n))
+
+
+
+
+
+
+
