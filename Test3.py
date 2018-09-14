9af618f2bce69d29dd372c7a809ff56d4dfe5624
Vladimir_T <42174061+Terehov2088@users.noreply.github.com>
2018-09-14 20:04:32 +0300

    12 сделал но бот ругается непойму на что

--- Test3.py
+++ Test3.py
@@ -1,6 +1,4 @@
 import random
-import math
-import string
 print("""Задача №12.  Для проверки остаточных знаний учеников после летних каникул,
 учитель младших классов решил начинать каждый урок с того, чтобы задавать каждому ученику пример из таблицы умножения, 
 но в классе 15 человек, а примеры среди них не должны повторяться. В помощь учителю напишите программу, 
@@ -29,54 +27,31 @@ def pretty_print_matrix(matrix):
 
 
 multiplication_table = [[(i+1)*(j+1) for i in range(10)] for j in range(10)]
-pretty_print_matrix(multiplication_table)
 print()
-example_table = [[0 for i in range(3)] for j in range(15)]
-pretty_print_matrix(example_table)
-print()
-def multiplication_example(matrix):
+
+
+def multiplication_example():
+    matrix = [[0 for i in range(3)] for j in range(15)]
     for i in range(len(matrix)):
-        for j in range(len(matrix[i])-1):
-            matrix[i][j] = random.randint(2, 9)
+        end = True
+        while end == True:
+            for j in range(len(matrix[i]) - 1):
+                matrix[i][j] = random.randint(2, 9)
+            matrix[i][j + 1] = (matrix[i][j]) * (matrix[i][j - 1])
+            # print(matrix[i][j + 1])
+            end = False
+            elem = matrix[i][j + 1]
+            idx = 0
+            for k in range(len(matrix)):
+                if elem == matrix[k][j + 1]:
+                    idx +=1
+            if idx > 1:
+                end = True
 
-        matrix[i][j+1] = (matrix[i][j])*(matrix[i][j-1])
-    pretty_print_matrix(matrix)
-
-    for elem in matrix:
-        for i in range((len(elem)-1), len(elem) ):
-            if matrix[elem.index(elem)][i] ==
-
-
-
-
-
-
-
-    return 0
-
+    return matrix
 
 
-multiplication_example(example_table)
+pretty_print_matrix(multiplication_example())
 
 
 
-# def find_saddle_points(a):
-#   """Finds saddle points in a, a 2 dimensional array.
-#
-#   Args:
-#     a: A 2 dimensional array in row-major (y, x) order.
-#   Returns:
-#     A list of (x, y) location of the saddle points.
-#   """
-#   # Holds a (value, column) tuple of the min value and its column for each row.
-#   row_mins = [min((a[row][col], col) for col in range(len(a[row]))) for row in range(len(a))]
-#   # Holds a (value, row) tuple of the max value and its row for each column.
-#   col_maxes = [max((a[row][col], row) for row in range(len(a))) for col in range(len(a[0]))]
-#
-#   ret = []
-#   for row, (row_min, col) in enumerate(row_mins):
-#     if col_maxes[col][1] == row:
-#       ret.append((row, col))
-#       print('Координаты седальной точки: %s' % ret)
-#   return ret
-# find_saddle_points(matrix_random)
