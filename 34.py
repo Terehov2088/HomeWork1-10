897ae3ac5070f2167cd1f98f00090abe6382a292
Vladimir_T <42174061+Terehov2088@users.noreply.github.com>
2018-09-18 13:52:23 +0300

    Сделал что бы в функции eat писал о заполнении желудка. 34 а старое, можно не смотреть

--- 34.py
+++ 34.py
@@ -59,75 +59,51 @@
  Когда Годзилла заполнит желудок на 90%, то Годзилла должен говорить он наелся и больше не может поедать людей.
 """
 import math
+import random
 
 class Godzilla():
     AGE_OF_ADULT_GODZILLA = 6
+    fill_stomach = 0
 
 
-    def __init__(self, name, age):
+    def __init__(self, name, age, stomach_value):
         self.name = name
         self.age = age
-        self.fill_stomach = [0] * math.ceil(int((self.stomach_value() / 75) + self.age/2))  #будем считать, что средний вес поедаемого студента  75 кг
-        # print(self.fill_stomach)
+        self.stomach_value = stomach_value
+        # self.total_stomach = self.godzilla_eat
         # print(self.fill_stomach.count(0))
         # print(len(self.fill_stomach))
 
     def pretty_print(self):
         print('Name:', self.name)
         print('Age:', self.age)
-        print('Stomach_value:', self.stomach_value())
-        print('Fill_stomach:', self.filled_stomach())
-        # print('Godzilla say:', self.godzilla_full())  # хотел сделать метод который выводил бы сообщение после того как будет указана текущее заполнение желудка, но пока никак не получилось
-
-    def stomach_value(self):
-        if self.age <= self.AGE_OF_ADULT_GODZILLA:
-            stomach_value = int(math.exp(self.age))
-        elif self.age > self.AGE_OF_ADULT_GODZILLA:
-            stomach_value = int(math.exp(self.AGE_OF_ADULT_GODZILLA) + (math.exp(self.AGE_OF_ADULT_GODZILLA-1)* self.age))
-        return stomach_value
-
+        print('Stomach_value:', self.stomach_value)
+        print('Fill_stomach:', self.fill_stomach)
 
     def godzilla_eat(self, mass):
-        a = len(self.fill_stomach)
-        for i in range(a):
-            if self.fill_stomach[i] == 0:
-                self.fill_stomach[i] = mass
-                break
-
-
-    def filled_stomach(self):
-        filled_stomach = 0
-        for elem in self.fill_stomach:
-            if filled_stomach < int(self.stomach_value() * 90 / 100) and (filled_stomach+elem) <= int(self.stomach_value() * 90 / 100):
-                filled_stomach += elem
-            else:
-                print("Годзилла %s наелся, и больше не может жрать студентов которые не сделали ДЗ" % self.name)
-                return filled_stomach
-        print("Годзилла %s не сытый, и еще может жрать студентов которые не сделали ДЗ" % self.name)
-        return filled_stomach
+        fill_stomach = self.fill_stomach
+        if fill_stomach < int(self.stomach_value*90/100) and int(fill_stomach + mass) < int(self.stomach_value*90/100):
+            fill_stomach = fill_stomach + mass
+            self.fill_stomach = fill_stomach
+            print("Годзилла %s не сытый, и еще может жрать студентов которые не сделали ДЗ" % self.name)
+        else:
+            print("Годзилла %s наелся, и больше не может жрать студентов которые не сделали ДЗ" % self.name)
+            return fill_stomach
+        return fill_stomach
 
 
 
 
-    # def godzilla_full(self):
-    #     if self.filled_stomach()
-    #     # if self.filled_stomach() < int(self.stomach_value() * 90 / 100):
-    #         print("Годзилла %s наелся, и больше не может жрать студентов которые не сделали ДЗ" % self.name)
-    #     else:
-    #         print("Годзилла %s не сытый, и еще может жрать студентов которые не сделали ДЗ" % self.name)
+godzilla1 = Godzilla('Morty', 10, 1000)
+for i in range(int(godzilla1.stomach_value/75)+int(godzilla1.age/2)):
+    godzilla1.godzilla_eat(random.randint(25, 100))
+
+godzilla2 = Godzilla("Rik", 20, 2500)
+for j in range(int(godzilla2.stomach_value / 75) + int(godzilla2.age / 2)):
+    godzilla2.godzilla_eat(random.randint(25, 100))
 
 
 
-
-
-godzilla1 = Godzilla('Morty', 10)
-godzilla1.godzilla_eat(850)
-godzilla1.godzilla_eat(50)
-godzilla1.godzilla_eat(880)
-
-godzilla2 = Godzilla("Rik", 20)
-godzilla2.godzilla_eat(1000)
-godzilla2.godzilla_eat(1000)
 
 godzilla1.pretty_print()
 print("=================")
---
+++ 34a.py
@@ -0,0 +1,80 @@
+"""
+Задача 34. Создать класс Годзилла. При создании Годзиллы указывается объем желудка.
+Написать для данного класса метод поедания людей eat.
+В данный метод должен передаваться объем съеденного человека и соответственно уменьшаться место в желудке.
+ Когда Годзилла заполнит желудок на 90%, то Годзилла должен говорить он наелся и больше не может поедать людей.
+"""
+import math
+
+class Godzilla():
+    AGE_OF_ADULT_GODZILLA = 6
+
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        self.fill_stomach = [0] * math.ceil(int((self.stomach_value() / 75) + self.age/2))  #будем считать, что средний вес поедаемого студента  75 кг
+        # print(self.fill_stomach)
+        # print(self.fill_stomach.count(0))
+        # print(len(self.fill_stomach))
+
+    def pretty_print(self):
+        print('Name:', self.name)
+        print('Age:', self.age)
+        print('Stomach_value:', self.stomach_value())
+        print('Fill_stomach:', self.filled_stomach())
+        # print('Godzilla say:', self.godzilla_full())  # хотел сделать метод который выводил бы сообщение после того как будет указана текущее заполнение желудка, но пока никак не получилось
+
+    def stomach_value(self):
+        if self.age <= self.AGE_OF_ADULT_GODZILLA:
+            stomach_value = int(math.exp(self.age))
+        elif self.age > self.AGE_OF_ADULT_GODZILLA:
+            stomach_value = int(math.exp(self.AGE_OF_ADULT_GODZILLA) + (math.exp(self.AGE_OF_ADULT_GODZILLA-1)* self.age))
+        return stomach_value
+
+
+    def godzilla_eat(self, mass):
+        a = len(self.fill_stomach)
+        for i in range(a):
+            if self.fill_stomach[i] == 0:
+                self.fill_stomach[i] = mass
+                break
+
+
+    def filled_stomach(self):
+        filled_stomach = 0
+        for elem in self.fill_stomach:
+            if filled_stomach < int(self.stomach_value() * 90 / 100) and (filled_stomach+elem) <= int(self.stomach_value() * 90 / 100):
+                filled_stomach += elem
+            else:
+                print("Годзилла %s наелся, и больше не может жрать студентов которые не сделали ДЗ" % self.name)
+                return filled_stomach
+        print("Годзилла %s не сытый, и еще может жрать студентов которые не сделали ДЗ" % self.name)
+        return filled_stomach
+
+
+
+
+    # def godzilla_full(self):
+    #     if self.filled_stomach()
+    #     # if self.filled_stomach() < int(self.stomach_value() * 90 / 100):
+    #         print("Годзилла %s наелся, и больше не может жрать студентов которые не сделали ДЗ" % self.name)
+    #     else:
+    #         print("Годзилла %s не сытый, и еще может жрать студентов которые не сделали ДЗ" % self.name)
+
+
+
+
+
+godzilla1 = Godzilla('Morty', 10)
+godzilla1.godzilla_eat(850)
+godzilla1.godzilla_eat(50)
+godzilla1.godzilla_eat(880)
+
+godzilla2 = Godzilla("Rik", 20)
+godzilla2.godzilla_eat(1000)
+godzilla2.godzilla_eat(1000)
+
+godzilla1.pretty_print()
+print("=================")
+godzilla2.pretty_print()
